d647811950a75bb894fd8fafa029b84c6865dcb6
enr <enrico.comiti@gmail.com>
2015-07-18 11:14:40 +0200

    keep process child alive in Windows

--- process_unix.go
+++ process_unix.go
@@ -7,13 +7,19 @@ import (
 	"syscall"
 )
 
+// ^C on Unix sends a signal to every process in the process group.
+// To keep alive the child process you have to put it in a different process group.
+// You do that by setting Setpgid to true in the Cmd.SysProcAttr field.
+// From Golang docs:
+// 	Setpgid: Set process group ID to new pid (SYSV setpgrp)
 func start(cmd *exec.Cmd) error {
+	keepAliveChild := true
 	if cmd.SysProcAttr == nil {
 		cmd.SysProcAttr = &syscall.SysProcAttr{
-			Setpgid: true,
+			Setpgid: keepAliveChild,
 		}
 	} else {
-		cmd.SysProcAttr.Setpgid = true
+		cmd.SysProcAttr.Setpgid = keepAliveChild
 	}
 	err := cmd.Start()
 	return err
--- process_windows.go
+++ process_windows.go
@@ -7,10 +7,18 @@ import (
 	"syscall"
 )
 
+// To keep alive the child process you have to put it in a different process
+// group.
+// You do that by setting CREATE_NEW_PROCESS_GROUP to true.
 func start(cmd *exec.Cmd) error {
-	if cmd.SysProcAttr == nil {
-		cmd.SysProcAttr = &syscall.SysProcAttr{
-		//Setpgid: true,
+	keepAliveChild := true
+	if keepAliveChild {
+		if cmd.SysProcAttr == nil {
+			cmd.SysProcAttr = &syscall.SysProcAttr{
+				CreationFlags: syscall.CREATE_NEW_PROCESS_GROUP,
+			}
+		} else {
+			cmd.SysProcAttr.CreationFlags = syscall.CREATE_NEW_PROCESS_GROUP
 		}
 	}
 	err := cmd.Start()
