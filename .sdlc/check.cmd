a2f10fae8b5dda7229dd18b07df8772817220507
enr <enrico.comiti@gmail.com>
2020-10-08 23:03:40 +0200

    sdlc

--- .sdlc/check.cmd
+++ .sdlc/check.cmd
@@ -11,37 +11,23 @@ cd ..
 set project_dir=%cd%
 
 set module_name=%REPO_HOST%/%REPO_OWNER%/%REPO_NAME%
-set exe_path=bin\%REPO_NAME%.exe
+set bin_dir=%project_dir%\bin
 
 echo script_name   %script_name%
 echo script_path   %script_path%
 echo script_dir    %script_dir%
 echo project_dir   %project_dir%
 echo module_name   %module_name%
-echo exe_path      %exe_path%
+echo bin_dir       %bin_dir%
+
+IF DEFINED SDLC_GO_VENDOR (
+    echo Using Go vendor
+    set GOPROXY=off
+)
 
 cd %project_dir%
 
-go test -race ./...
-go test -v -cover ./...
+call go test -mod vendor -race ./...
+call go test -mod vendor -cover ./...
 
-REM for /f %%x in ('dir /AD /B /S lib') do (
-REM     echo --- %%x
-REM     cd %%x
-REM     go test -mod vendor -cover ./...
-REM )
-REM
-REM cd %project_dir%
-REM
-REM IF EXIST %exe_path% DEL /F %exe_path%
-REM
-REM @echo ON
-REM call go build -mod vendor -ldflags "-s -X %module_name%/lib/core.Version=%APP_VERSION% -X %module_name%/lib/core.BuildTime=%TIMESTAMP% -X %module_name%/lib/core.GitCommit=win-dev-commit" ^
-REM   -o %exe_path% "%module_name%/cmd/%REPO_NAME%"
-REM
-REM @echo OFF
-REM for /f %%x in ('dir /AD /B /S cmd') do (
-REM     echo --- %%x
-REM     cd %%x
-REM     go test -mod vendor -cover ./...
-REM )
+
--- .sdlc/config.bat
+++ .sdlc/config.bat
@@ -2,5 +2,8 @@ set REPO_HOST=github.com
 set REPO_OWNER=enr
 set REPO_NAME=runcmd
 
+set SDLC_GO_VENDOR=vendor
+set SDLC_GO_BASE=
+
 set TIMESTAMP=win-dev-timestamp
 set APP_VERSION=win-dev
